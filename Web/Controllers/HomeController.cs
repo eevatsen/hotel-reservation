1082217376d72914d9b3986f548102a8a30522cd
Vanya Lutsenko <vanyalucenko3@gmail.com>
2025-12-24 14:19:11 +0200

    Add authentication UI and controllers with Bootstrap

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -21,10 +21,10 @@ public class HomeController : Controller
         var hotels = await _context.Hotels.Include(h => h.Rooms).ToListAsync();
         return View(hotels);
     }
-
-    //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    //public IActionResult Error()
-    //{
-    //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    //}
+        
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
 }
